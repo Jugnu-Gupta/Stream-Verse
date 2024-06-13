35c27079ce988646bcd8c242f89aa34d2277da3e
jugnugupta33 <jugnugupta33@gmail.com>
2024-06-13 12:27:49 +0530

    added fileFilter in multer & limits on file size

--- Backend/src/middlewares/multer.middleware.js
+++ Backend/src/middlewares/multer.middleware.js
@@ -10,5 +10,41 @@ const storage = multer.diskStorage({
 });
 
 
-// export const upload = multer({ storage: storage, limits: { fileSize: 1000000 * 5 } });
-export const upload = multer({ storage: storage });
+// File filter to allow only certain file types
+const imagefileFilter = (req, file, cb) => {
+    const allowedTypes = /jpeg|jpg|png/;
+    const extname = allowedTypes.test(path.extname(file.originalname).toLowerCase());
+    const mimetype = allowedTypes.test(file.mimetype);
+
+    if (extname && mimetype) {
+        return cb(null, true);
+    } else {
+        cb(new Error('Only images of types jpeg, jpg and png are allowed'));
+    }
+};
+
+const videofileFilter = (req, file, cb) => {
+    const allowedTypes = /mp4|mov|mpg|mpeg4|wmv|flv|webm|avi|mkv/;
+    const extname = allowedTypes.test(path.extname(file.originalname).toLowerCase());
+    const mimetype = allowedTypes.test(file.mimetype);
+
+    if (extname && mimetype) {
+        return cb(null, true);
+    } else {
+        cb(new Error('Only videos of type mp4, flv, avi, mkv, mov, wmv, webm, mpg and mpeg4 are allowed'));
+    }
+};
+
+export const uploadImage = multer({
+    storage: storage,
+    fileFilter: imagefileFilter,
+    // Limit image size to 10 MB
+    limits: { fileSize: 1024 * 1024 * 10 }
+});
+
+export const uploadVideo = multer({
+    storage: storage,
+    fileFilter: videofileFilter,
+    // Limit video size to 100 MB
+    limits: { fileSize: 1024 * 1024 * 100 }
+});
