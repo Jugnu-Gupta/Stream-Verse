c9847c85ffe55df34facb5cb54c28e8a8234ee57
jugnugupta33 <jugnugupta33@gmail.com>
2024-08-02 20:29:33 +0530

    Update responsive grid layout in ChannelPlaylists, ChannelVideos, and Home components

--- frontend/src/pages/Channel/Videos/ChannelVideos.tsx
+++ frontend/src/pages/Channel/Videos/ChannelVideos.tsx
@@ -2,35 +2,27 @@ import React from "react";
 import ChannelVideosCard from "./ChannelVideoCard";
 import { twMerge } from "tailwind-merge";
 import useWindowWidth from "../../../hooks/useWindowWidth";
-import { useSelector } from "react-redux";
-import { RootState } from "../../../context/store";
 
-const handleWidth = (width: number, isVisible: boolean) => {
-	console.log(width, isVisible);
-	if (width <= 550) {
+const handleWidth = (width: number) => {
+	if (width <= 600) {
 		return "grid-cols-1";
 	} else if (width <= 875) {
 		return "grid-cols-2";
-	} else if (width <= 1200) {
+	} else if (width <= 1250) {
 		return "grid-cols-3";
-	} else if (!isVisible) {
-		return "grid-cols-4";
 	} else {
-		return "grid-cols-3";
+		return "grid-cols-4";
 	}
 };
 
 const ChannelVideos: React.FC = () => {
 	const windowWidth = useWindowWidth();
-	const isVisible: boolean = useSelector(
-		(state: RootState) => state.navbar.showNavbar
-	);
 
 	return (
 		<div
 			className={twMerge(
 				"grid px-4 w-full justify-items-center mt-4",
-				handleWidth(windowWidth, isVisible)
+				handleWidth(windowWidth)
 			)}>
 			<ChannelVideosCard />
 			<ChannelVideosCard />
--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,36 +1,28 @@
 import React from "react";
 import VideoCardView from "./VideoCardView";
 import { twMerge } from "tailwind-merge";
-import { useSelector } from "react-redux";
-import { RootState } from "../../context/Store";
 import useWindowWidth from "../../hooks/useWindowWidth";
 
-const handleWidth = (width: number, isVisible: boolean) => {
-	console.log(width, isVisible);
-	if (width <= 550) {
+const handleWidth = (width: number) => {
+	if (width <= 600) {
 		return "grid-cols-1";
 	} else if (width <= 875) {
 		return "grid-cols-2";
-	} else if (width <= 1200) {
+	} else if (width <= 1250) {
 		return "grid-cols-3";
-	} else if (!isVisible) {
-		return "grid-cols-4";
 	} else {
-		return "grid-cols-3";
+		return "grid-cols-4";
 	}
 };
 
 const Home: React.FC = () => {
 	const windowWidth = useWindowWidth();
-	const isVisible: boolean = useSelector(
-		(state: RootState) => state.navbar.showNavbar
-	);
 
 	return (
 		<div
 			className={twMerge(
 				"grid m-2 w-full justify-items-center",
-				handleWidth(windowWidth, isVisible)
+				handleWidth(windowWidth)
 			)}>
 			<VideoCardView />
 			<VideoCardView />
