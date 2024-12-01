f9885a51fb54b0ff71f97dee710b5a3b2a4c8a48
jugnugupta33 <jugnugupta33@gmail.com>
2024-12-02 01:31:11 +0530

    Enhance comparePaths utility with additional user and admin checks

--- frontend/src/utils/ComaprePaths.ts
+++ frontend/src/utils/ComaprePaths.ts
@@ -2,18 +2,25 @@ export const comparePaths = (link: string, curPage: string) => {
 	if (link == curPage) {
 		return true;
 	} else {
+		const curUser: string = link.split("/")?.[0];
+		const channelAdmin: string =
+			"@" + localStorage.getItem("channelAdmin") || "";
+
+		console.log("curUser:", curUser);
+		console.log("channelAdmin:", channelAdmin);
 		// console.log("link:", link);
-		// console.log("curPage:", curPage);
+		console.log("curPage:", curPage);
 		const path = "/" + link.split("/").pop();
-		// console.log("path:", path);
+		console.log("path:", path);
 		if (
 			path === "/videos" &&
+			curUser === channelAdmin &&
 			["/videos", "/playlists", "/tweets", "/subscribed"].includes(
 				curPage
 			)
 		) {
 			return true;
-		} else if (path === curPage) {
+		} else if (path === curPage && path === "/dashboard") {
 			return true;
 		} else {
 			return false;
