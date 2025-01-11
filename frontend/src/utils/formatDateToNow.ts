48679ccc37cbfd8f766e5f54481ee1fb6a55faa8
jugnugupta33 <jugnugupta33@gmail.com>
2025-01-11 21:21:43 +0530

    Revert "Revert "change export style of  formatDateToNow for improved clarity""
    This reverts commit 05c7392cedb23e8bb9779b49b707a95b90832e36.

--- frontend/src/utils/formatDateToNow.ts
+++ frontend/src/utils/formatDateToNow.ts
@@ -1,6 +1,8 @@
 import { formatDistanceToNow } from "date-fns";
 
-export const formatDateToNow = (date: Date | undefined): string => {
+const formatDateToNow = (date: Date | undefined): string => {
 	if (!date) return formatDateToNow(new Date());
 	return formatDistanceToNow(new Date(date), { addSuffix: true });
 };
+
+export { formatDateToNow };
