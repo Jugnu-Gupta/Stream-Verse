d7746cc76310bb483b54bef6275df4316c0a4ddd
jugnugupta33 <jugnugupta33@gmail.com>
2025-01-23 02:42:58 +0530

    Change static file serving directory from 'public' to 'tmp'

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -8,7 +8,7 @@ dotenv.config();
 app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.json({ limit: "16kb" }));
-app.use(express.static("public"));
+app.use(express.static("tmp"));
 app.use(cookieParser());
 
 // routes import
