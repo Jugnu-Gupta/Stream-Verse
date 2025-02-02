3f9c61690ee1377bbb4368f37e2c94d7e0dfb21f
jugnugupta33 <jugnugupta33@gmail.com>
2025-02-02 15:22:42 +0530

    Fix optional chaining for videoId and thumbnail in VideoCardView and VideoListView components

--- frontend/src/pages/Home/VideoCardView.tsx
+++ frontend/src/pages/Home/VideoCardView.tsx
@@ -16,14 +16,14 @@ const VideoCardView: React.FC<VideoCardViewProps> = ({ videoInfo }) => {
 	const ownerFullName = videoInfo?.owner?.fullName || "Channel Name";
 	const ownerUserName = videoInfo?.owner?.userName;
 	const title = videoInfo.title;
-	const videoId = videoInfo._id;
+	const videoId = videoInfo?._id;
 	const avatar = videoInfo?.owner?.avatar?.url || generateAvatar(ownerFullName, "0078e1", "ffffffcc");
 	const navigate = useNavigate();
 
 	return (
 		<div className="flex flex-col gap-2 p-2 group max-w-[400px] overflow-hidden w-full">
 			<div className="overflow-hidden rounded-xl relative" onClick={() => navigate(`/video/${videoId}`)}>
-				<img src={videoInfo.thumbnail.url} alt="thumbnail" loading='lazy'
+				<img src={videoInfo?.thumbnail?.url} alt="thumbnail" loading='lazy'
 					className="rounded-xl aspect-video object-cover group-hover:scale-110 duration-300 relative z-0 w-full" />
 				<p className="px-1 py-[1px] absolute z-10 bottom-2 right-2 text-xs text-primary-text rounded-md bg-black bg-opacity-70">
 					{duration}
--- frontend/src/pages/Search/VideoListView.tsx
+++ frontend/src/pages/Search/VideoListView.tsx
@@ -18,13 +18,13 @@ const VideoListView: React.FC<VideoListViewProps> = ({ videoInfo }) => {
 	const ownerFullName = videoInfo?.owner?.fullName || "ownerName";
 	const title = videoInfo?.title;
 	const avatar = videoInfo?.owner?.avatar?.url || generateAvatar(ownerFullName, "0078e1", "ffffffcc");
-	const videoId = videoInfo._id;
+	const videoId = videoInfo?._id;
 	const navigate = useNavigate();
 
 	return (
 		<div className="flex gap-4 p-2 group w-full justify-start">
 			<div className="overflow-hidden rounded-xl max-w-md w-fit h-fit relative" onClick={() => navigate(`/video/${videoId}`)}>
-				<img src={videoInfo.thumbnail.url} alt="thumbnail" loading='lazy'
+				<img src={videoInfo?.thumbnail?.url} alt="thumbnail" loading='lazy'
 					className="rounded-xl aspect-video object-cover group-hover:scale-110 duration-300 relative z-0 w-96" />
 				<p className="px-1 py-[1px] z-[1] absolute bottom-2 right-2 text-xs text-white rounded-md bg-black bg-opacity-70">
 					{duration}
